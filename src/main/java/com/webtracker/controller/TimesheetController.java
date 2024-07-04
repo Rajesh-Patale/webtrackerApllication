6a544fe1514d6f2830b7d04d3e06efa31e18c137
rajpatale <rajpatale88@gmail.com>
2024-07-04 12:55:47 +0530

    added cross-origin over controller class.

--- src/main/java/com/webtracker/controller/TimesheetController.java
+++ src/main/java/com/webtracker/controller/TimesheetController.java
@@ -3,20 +3,14 @@ package com.webtracker.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.webtracker.entity.Timesheet;
 import com.webtracker.service.TimesheetService;
 
 @RestController
 @RequestMapping("/api/timesheets")
+@CrossOrigin("*")
 public class TimesheetController {
 	
 	
