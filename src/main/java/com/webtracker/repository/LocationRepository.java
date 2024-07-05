eca392f48ac225d1c0f115071cd0ca3b7e01bee1
rajpatale <rajpatale88@gmail.com>
2024-07-05 12:07:03 +0530

    Removed all unwanted classes.

--- src/main/java/com/webtracker/repository/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webtracker.repository;
-
-import com.webtracker.entity.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationRepository extends JpaRepository<Location,Long> {
-}
